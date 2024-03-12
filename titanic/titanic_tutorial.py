eab59997eea1b5a3eb1bf3ad8279e44be9c22eb7
yxy7721 <yxy7721@qq.com>
2024-03-12 10:40:29 +0800

    u

---
+++ titanic/titanic_tutorial.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 26 09:42:26 2023
+
+@author: yangxy
+"""
+
+# data analysis and wrangling
+import pandas as pd
+import numpy as np
+import random as rnd
+
+# visualization
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# machine learning
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import Perceptron
+from sklearn.linear_model import SGDClassifier
+from sklearn.tree import DecisionTreeClassifier
+
+train_df = pd.read_csv(r'D:/desktop/titanic/train.csv')
+test_df = pd.read_csv(r'D:/desktop/titanic/test.csv')
+combine = [train_df, test_df]
+
+train_df.describe(include=['O'])
+
+train_df[['Pclass', 'Survived']].groupby(['Pclass'], as_index=False).mean().sort_values(by='Survived', ascending=False)
+
+g = sns.FacetGrid(train_df, col='Survived')
+g.map(plt.hist, 'Age', bins=20)
+
+
+grid = sns.FacetGrid(train_df, col='Survived', row='Pclass', size=2.2, aspect=1.6)
+grid.map(plt.hist, 'Age', alpha=.5, bins=20)
+grid.add_legend();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
